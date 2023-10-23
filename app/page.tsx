86d291f17882cd8d32f16e25e8347b7583bef1bb
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-10-23 21:45:27 +0600

    sharp package added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import AvatarUpload from "@/components/image-upload/profile";
-import { ProductModal } from "@/components/modal";
-
-
 
 const Home = () => {
   return (
     <main className="container mx-auto bg-white-500/50 min-h-screen py-10">
-      <h1 className="text-center text-4xl underline underline-offset-8 my-10">Home Page</h1>
-      {/* <AvatarUpload /> */}
-      <ProductModal/>
+      <h1 className="text-center text-4xl underline underline-offset-8 my-10">
+        Home Page
+      </h1>
+      <AvatarUpload />
     </main>
   );
 };
